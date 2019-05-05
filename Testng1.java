7d7422e7b558964a36c9271b8021452077b70ea7
aparnasp <aparnasp@users.noreply.github.com>
2019-05-05 18:04:26 +0530

    Update Testng1.java

--- Testng1.java
+++ Testng1.java
@@ -22,6 +22,12 @@ public class Testng1 {
 		System.out.println("THIS IS A 3nd TESTNG CLASS FOR DEMO");
 		
 	}
+	
+	@Test
+	public static void method4() {
+		System.out.println("THIS IS A 4th TESTNG CLASS FOR DEMO");
+		
+	}
 	@Test
 	public static void method5() {
 		System.out.println("THIS IS A 5th TESTNG CLASS FOR DEMO");
